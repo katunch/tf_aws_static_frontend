7c01c4fa919dd3fb4fd876ba6d4e5effe31edf52
Silas Knobel <silas.knobel@katun.ch>
2025-04-16 09:22:20 +0200

    added more outputs

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,18 @@ output "cloudfront_distribution_id" {
   sensitive   = false
 }
 
+output "cloudfront_distribution_domain_name" {
+  value       = aws_cloudfront_distribution.default.domain_name
+  description = "The domain name of the CloudFront distribution"
+  sensitive   = false
+}
+
+output "cloudfront_distribution_hosted_zone_id" {
+  value       = aws_cloudfront_distribution.default.hosted_zone_id
+  description = "The hosted zone ID of the CloudFront distribution"
+  sensitive   = false
+}
+
 output "pipeline_access_key_id" {
   value       = aws_iam_access_key.pipeline.id
   description = "The ID of the IAM access key for using in a pipeline to deploy the site"
